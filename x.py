8781d3283891109de9e237e38ac8c95c3f2d5033
lokarakesh <114563209+lokarakesh@users.noreply.github.com>
2024-12-08 22:25:09 +0530

    Add files via upload

---
+++ x.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": None,
+   "id": "9395bd5c-d6e6-423d-82f7-b1cced7b69f3",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
